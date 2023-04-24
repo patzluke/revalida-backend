17addd043ee0ab14d962118a973d056b22dfdfa7
patzluke <patzluke@gmail.comm>
2023-04-24 13:41:42 +0800

    modified department and positions table creation.

--- src/main/java/config/users.sql
+++ src/main/java/config/users.sql
@@ -11,7 +11,7 @@ create table departments (
 insert into departments values(0, 'N/A');
 drop sequence if exists departments_sequence;
 create sequence departments_sequence as int increment by 1 start with 1;
-alter table departments alter column departments_id set default nextval('departments_sequence');
+alter table departments alter column department_id set default nextval('departments_sequence');
 
 
 drop table if exists positions;
@@ -23,7 +23,6 @@ insert into positions values(0, 'N/A');
 drop sequence if exists position_sequence;
 create sequence position_sequence as int increment by 1 start with 1;
 alter table positions alter column position_id set default nextval('position_sequence');
-
 
 drop table if exists users;
 create table users (
@@ -39,8 +38,8 @@ create table users (
     birth_date date,
     gender varchar(10),
     position_id int,
-    foreign key(dept_id) references departments(department_id),
-    foreign key(position_id) references positions(position_id)
+    foreign key(dept_id) references departments(department_id) on delete cascade,
+    foreign key(position_id) references positions(position_id) on delete cascade
 );
 
 drop table if exists user_tokens;
@@ -53,11 +52,11 @@ create table user_tokens (
 insert into departments(department_name) values ('IT');
 insert into departments(department_name) values ('HR');
 
-insert into positions(position_name) values ('Developer');
-insert into positions(position_name) values ('HELPER');
+insert into positions(position_name) values ('DEVELOPER');
+insert into positions(position_name) values ('Programmer');
 
 insert into users(email, mobile_number, password, user_type, first_name, middle_name, last_name, dept_id, birth_date, gender, position_id) 
 values('ally@gmail.com', '9178192726', '123456', 'admin', 'ally', 'artuz', 'astrero', 1, '2015-07-25', 'female', 1);
 
 insert into users(email, mobile_number, password, user_type, first_name, middle_name, last_name, dept_id, birth_date, gender, position_id) 
-values('patz@gmail.com', '9178192726', '123456', 'employee', 'patz', 'artuz', 'astrero', 1, '2015-07-25', 'female', 1);
+values('patz@gmail.com', '9178192725', '123456', 'employee', 'patz', 'artuz', 'astrero', 1, '2015-07-25', 'female', 1);
