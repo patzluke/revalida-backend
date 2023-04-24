2162ed0b834f84e6e4861ccd1f8214a8c3101b7f
patzluke <patzluke@gmail.comm>
2023-04-24 15:13:49 +0800

    added users.sql trigger functions by ados

--- src/main/java/config/users.sql
+++ src/main/java/config/users.sql
@@ -8,7 +8,7 @@ create table departments (
 	department_id int primary key,
 	department_name varchar(50) unique
 );
-insert into departments values(0, 'N/A');
+insert into departments values(0, 'TBD');
 drop sequence if exists departments_sequence;
 create sequence departments_sequence as int increment by 1 start with 1;
 alter table departments alter column department_id set default nextval('departments_sequence');
@@ -19,7 +19,7 @@ create table positions (
 	position_id int primary key,
 	position_name varchar(50) unique
 );
-insert into positions values(0, 'N/A');
+insert into positions values(0, 'TBD');
 drop sequence if exists position_sequence;
 create sequence position_sequence as int increment by 1 start with 1;
 alter table positions alter column position_id set default nextval('position_sequence');
@@ -60,3 +60,31 @@ values('ally@gmail.com', '9178192726', '123456', 'admin', 'ally', 'artuz', 'astr
 
 insert into users(email, mobile_number, password, user_type, first_name, middle_name, last_name, dept_id, birth_date, gender, position_id) 
 values('patz@gmail.com', '9178192725', '123456', 'employee', 'patz', 'artuz', 'astrero', 1, '2015-07-25', 'female', 1);
+
+CREATE OR REPLACE FUNCTION set_dept_id_to_zero()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE users SET dept_id = 0 WHERE dept_id = OLD.department_id;
+  RETURN OLD;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION set_position_id_to_zero()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE users SET position_id = 0 WHERE position_id = OLD.position_id;
+  RETURN OLD;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER set_dept_id_to_zero_trigger
+BEFORE DELETE ON departments
+FOR EACH ROW
+EXECUTE FUNCTION set_dept_id_to_zero();
+
+CREATE TRIGGER set_position_id_to_zero_trigger
+BEFORE DELETE ON positions
+FOR EACH ROW
+EXECUTE FUNCTION set_position_id_to_zero();
